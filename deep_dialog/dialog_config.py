928ab8565a44df8cd8192bdddabf562c6a9e34d8
igoingdown <fycjmingxing@126.com>
2018-09-19 11:00:46 +0800

    dialog state to chinese  and commit time: 2018-09-19 11:00:46.

--- deep_dialog/dialog_config.py
+++ deep_dialog/dialog_config.py
@@ -5,18 +5,17 @@
 '''
 
 all_acts = ['request', 'inform']
-inform_slots = ['actor','critic_rating','genre','mpaa_rating','director','release_year']
-chinese_inform_slots = ['演员','IMDB评分','类别','MPAA评级','导演','发行时间']
+# TODO: 将系统内部存储的状态也转为中文
+# inform_slots = ['actor','critic_rating','genre','mpaa_rating','director','release_year']
+# sys_request_slots = ['actor', 'critic_rating', 'genre', 'mpaa_rating', 'director', 'release_year']
+# start_dia_acts = {
+#     'greeting':[],
+#     'request':['moviename', 'starttime', 'theater', 'city', 'state', 'date', 'genre', 'ticket', 'numberofpeople', 'numberofkids']
+# }
 
-sys_request_slots = ['actor', 'critic_rating', 'genre', 'mpaa_rating', 'director', 'release_year']
-chinese_sys_request_slots = ['演员','IMDB评分','类别','MPAA评级','导演','发行时间']
-
-start_dia_acts = {
-    #'greeting':[],
-    'request':['moviename', 'starttime', 'theater', 'city', 'state', 'date', 'genre', 'ticket', 'numberofpeople', 'numberofkids']
-}
-chinese_start_dia_acts={'request':['影片名称', '首映时间', '影院', '城市', '省份', '时间', '类别', '余票', '观影人数', '观影儿童数']
-}
+inform_slots = [u'主演',u'评分',u'类别',u'评级',u'导演',u'发行年份']
+sys_request_slots = [u'主演',u'评分',u'类别',u'评级',u'导演',u'发行年份']
+start_dia_acts={'request':[u'片名', u'首映时间', u'影院', u'城市', u'省份', u'时间', u'类别', u'余票', u'观影人数', u'观影儿童数']}
 
 #reward information
 FAILED_DIALOG_REWARD = -1
