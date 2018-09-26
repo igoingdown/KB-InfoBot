f49e945c04ecaf1ff42b3f27c4bcce3c0e14a47a
igoingdown <fycjmingxing@126.com>
2018-09-26 20:55:49 +0800

    debug dict generating  and commit time: 2018-09-26 20:55:49.

--- deep_dialog/agents/feature_extractor.py
+++ deep_dialog/agents/feature_extractor.py
@@ -54,6 +54,7 @@ class FeatureExtractor:
                     tokens = to_tokens(ele)
                     for ngram in tokens:
                         if ngram.strip() != "" and ngram not in self.grams:
+                            print ngram
                             self.grams[ngram] = self.n
                             self.n += 1
 
@@ -100,6 +101,7 @@ class FeatureExtractor:
                 for ngram in to_tokens(line.strip()):
                     if ngram.strip() != "" and ngram not in self.grams:
                         self.grams[ngram] = self.n
+                        print ngram
                         self.n += 1
 
         # try:
