291827cd0b127e16203eaea57bee31dad1f28209
igoingdown <fycjmingxing@126.com>
2018-10-25 08:26:35 +0800

    interact unicode bug  and commit time: 2018-10-25 08:26:35.

--- interact.py
+++ interact.py
@@ -136,8 +136,8 @@ user_sim = CmdUser(movie_kb, act_set, slot_set, None, max_turn, err_prob, db_ful
         fdict_path = 'data/'+params['db']+'/fdict_2.p')
 
 # load all agents
-print WELCOME
-print u"载入模型中，请稍后……"
+print WELCOME.encode("utf8") if WELCOME is not None and type(WELCOME) == unicode else WELCOME
+# print u"载入模型中，请稍后……"
 agent_type = agent_map[params['agent']]
 for k,v in agent_params[agent_type].iteritems():
     params[k] = v
